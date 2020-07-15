547b70d646c3435fb8d7c5899fe123a623f4906a
up-tea <sacrament_ohio@mbr.nifty.com>
2020-07-16 07:19:18 +0900

    slot まとめ

--- src/components/ScopedSlotChild.vue
+++ src/components/ScopedSlotChild.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="named-slot-child">
     <h2>{{message}}</h2>
+    <h3>:ai="子のリアクティブデータのobject"でobjectを親から参照できるようにするパターン</h3>
+
+    <slot name="first" :actress="ai">
+      <p>first slot</p>
+    </slot>
+
+    <slot name="second" :actress="ai">
+      <p>second slot</p>
+    </slot>
+
+    <h3>:namae="子のリアクティブデータの文字列"で文字列を親から参照できるようにするパターン</h3>
+
+    <slot name="third" :namae="namae">
+      <p>third slot</p>
+    </slot>
+
+    <slot name="fourth" :namae="namae">
+      <p>fourth slot</p>
+    </slot>
   </div>
 </template>
 
@@ -8,7 +27,12 @@
 export default {
   data() {
     return {
-      message: "スコープ付きスロット"
+      message: "スコープ付きスロット",
+      ai: {
+        name: "上原亜衣",
+        age: 27
+      },
+      namae: "上原亜衣"
     };
   }
 };
